d3335f73aeb50f6983d2c001d37c96eb404dac0b
Duncan Kariuki <duncankariuki60@gmail.com>
2018-12-18 10:19:14 +0300

    added a color theme and separated the bussiness logic from the user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
+  // Bussiness Logic
   function roll() {
     return Math.floor(6 * Math.random()) + 1;
   };
+
   var playerRoll = 0;
   var playerRoll1 = 0;
   var player;
@@ -19,49 +20,51 @@ $(document).ready(function() {
     }
   };
 
-  $("#newGame").click(function () {
-    playerRoll = 0;
-    total = 0;
-    player1Total = 0;
-    player2Total = 0;
-    myTurn1 = 0;
-    myTurn2 = 0;
-    $("#top").val("");
-    $("#bottom").val("");
-    $("#topRight").val("");
-    $("#bottomRight").val("");
-  })
+  // User Interface
+  $(document).ready(function() {
+    $("#newGame").click(function() {
+      playerRoll = 0;
+      total = 0;
+      player1Total = 0;
+      player2Total = 0;
+      myTurn1 = 0;
+      myTurn2 = 0;
+      $("#top").val("");
+      $("#bottom").val("");
+      $("#topRight").val("");
+      $("#bottomRight").val("");
+    })
 
-  $("#player1").click(function player1() {
-    player = "Player 2";
-    myTurn2 = 0;
-    playerRoll = 0;
-    $("#bottomRight").val("");
-    rollNunber();
-    total += myTurn1;
-    myTurn1 = playerRoll;
-    $("#bottom").val(myTurn1);
-    $("#top").val(total);
-  });
+    $("#player1").click(function player1() {
+      player = "Player 2";
+      myTurn2 = 0;
+      playerRoll = 0;
+      $("#bottomRight").val("");
+      rollNunber();
+      total += myTurn1;
+      myTurn1 = playerRoll;
+      $("#bottom").val(myTurn1);
+      $("#top").val(total);
+    });
 
-  $("#player2").click(function player2() {
-    player = "Player 1";
-    myTurn1 = 0;
-    playerRoll = 0;
-    $("#bottom").val("");
-    rollNunber();
-    player2Total += myTurn2;
-    myTurn2 = playerRoll;
-    $("#bottomRight").val(myTurn2);
-    $("#topRight").val(player2Total);
-  });
+    $("#player2").click(function player2() {
+      player = "Player 1";
+      myTurn1 = 0;
+      playerRoll = 0;
+      $("#bottom").val("");
+      rollNunber();
+      player2Total += myTurn2;
+      myTurn2 = playerRoll;
+      $("#bottomRight").val(myTurn2);
+      $("#topRight").val(player2Total);
+    });
 
-  $("#hold").click(function(){
-    var player1result = total += myTurn1;
-    $("#top").val(player1result);
-    $("#bottom").val("");
-    var player2result = player2Total += myTurn2;
-    $("#topRight").val(player2result);
-    $("#bottomRight").val("");
+    $("#hold").click(function() {
+      var player1result = total += myTurn1;
+      $("#top").val(player1result);
+      $("#bottom").val("");
+      var player2result = player2Total += myTurn2;
+      $("#topRight").val(player2result);
+      $("#bottomRight").val("");
+    });
   });
-});
